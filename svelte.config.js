a3b1b8dea1e31bc9c889649f418914f7d6fe673d
Chad Christopher <devluxv20@gmail.com>
2022-09-19 21:25:47 -0700

    Create utils folder and alias, create lib folder Assignees: Labels: Fixes #3

--- svelte.config.js
+++ svelte.config.js
@@ -8,9 +8,20 @@ const config = {
 	preprocess: preprocess({
 		postcss: true
 	}),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			// this will match a file
+			'my-file': 'path/to/my-file.js',
+
+			// this will match a directory and its contents
+			// (`my-directory/x` resolves to `path/to/my-directory/x`)
+			'$utils/': './src/utils/',
+
+			// an alias ending /* will only match
+			// the contents of a directory, not the directory itself
+			'$utils/*': './src/utils/*'
+		}
 	}
 };
 
