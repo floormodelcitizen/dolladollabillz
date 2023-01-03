869b05e48cfac792186355f2a7445dfec78aeaba
Chad Christopher <devluxv20@gmail.com>
2023-01-02 17:09:44 -0800

    Add breakpoints Fixes #13

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,3 +3,10 @@
 
 <h1 class="text-3xl flex font-bold underline">Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>
+  <ul class="flex p-8">
+    <li><a href="/invoices" class="px-2">Invoices</a></li>
+    <li><a href="/clients" class="px-2">Clients</a></li>
+  </ul>
+</div>
